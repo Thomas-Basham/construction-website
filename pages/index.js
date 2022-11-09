506c1cc9a1aa36231e4ffa2a82df2c9568882042
Thomas Basham <Bashamtg@gmail.com>
2022-11-08 19:28:21 -0800

    added photo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { Row, Col, Container } from "react-bootstrap";
 import ImageGallery from "react-image-gallery";
 import orangeCouch from "/public/stock/orange-couch.jpg";
+import patio from "/public/stock/patio.jpg";
 export default function Home(props) {
-  const images = [orangeCouch.src];
+  const images = [orangeCouch, patio];
+
   const imageGalleryData = images.map((image) => {
-    return { original: image };
+    return { original: image.src };
   });
   return (
     <>
